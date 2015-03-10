d959c99b243a850a90291d464f39befc1a47ba0e
bengal2009 <bennylin@outlook.com>
2015-03-10 13:38:33 +0800

    office

--- app/src/main/java/com/example/blin/bennytest/GPS/GPSDEMO.java
+++ app/src/main/java/com/example/blin/bennytest/GPS/GPSDEMO.java
@@ -3,24 +3,58 @@ package com.example.blin.bennytest.GPS;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewSwitcher.ViewFactory;
 
 import com.example.blin.benlib.BenGps;
+import com.example.blin.benlib.BenUtil;
 import com.example.blin.bennytest.R;
 
 public class GPSDEMO extends ActionBarActivity {
     String TAG = "GPSDEMO";
+    private static final String[] TEXTS = { "Image #1", "Image #2", "Image #3" };
+    private int mPosition = 0;
+    private TextSwitcher mTextSwitcher;
+    private TextSwitcher mTextSwitcher2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        BenUtil A1=new BenUtil();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gpsdemo);
+        mTextSwitcher = (TextSwitcher) findViewById(R.id.textSwitcher);
+        mTextSwitcher2 = (TextSwitcher) findViewById(R.id.textSwitcher2);
+        A1.BenAnimateText(this,mTextSwitcher2);
+        mTextSwitcher.setFactory(new ViewFactory() {
+            @Override
+            public View makeView() {
+                TextView textView = new TextView(getApplication());
+                textView.setGravity(Gravity.CENTER);
+                return textView;
+            }
+        });
+
+        mTextSwitcher.setInAnimation(this, android.R.anim.fade_in);
+        mTextSwitcher.setOutAnimation(this, android.R.anim.fade_out);
+
 
     }
+    public void onSwitch(View view) {
+        mTextSwitcher.setText(TEXTS[mPosition]);
+        mPosition = (mPosition + 1) % TEXTS.length;
+    }
+    public void onSwitch2(View view) {
+        mTextSwitcher2.setText(TEXTS[mPosition]);
+        mPosition = (mPosition + 1) % TEXTS.length;
+    }
 
-    public void GpsTest(View v)
+    public void TestGPS(View v)
     {
         String address = "";
         BenGps.GPSService mGPSService = new BenGps.GPSService(this);
