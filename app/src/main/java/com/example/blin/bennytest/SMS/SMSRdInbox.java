c59f7bd4f779ca7969ef2f3340449d952fffbc17
bengal2009 <bennylin@outlook.com>
2015-03-13 17:23:38 +0800

    office

---
+++ app/src/main/java/com/example/blin/bennytest/SMS/SMSRdInbox.java
@@ -0,0 +1,154 @@
+package com.example.blin.bennytest.SMS;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+
+import com.example.blin.bennytest.R;
+/*
+所有文件夹：content://sms/all
+        收件箱：content://sms/inbox
+        已发送：content://sms/sent
+        草稿：content://sms/draft
+        发件箱：content://sms/outbox
+        发送失败：content://sms/failed
+        排队消息:content://sms/queued
+        未送达：content://sms/undelivered
+
+        对话：content://sms/conversations
+*/
+
+public class SMSRdInbox extends Activity implements View.OnClickListener {
+    private static final Uri SMS_INBOX = Uri.parse("content://sms/inbox");
+    //  GUI Widget
+    Button btnSent, btnInbox, btnDraft;
+    TextView lblMsg, lblNo;
+    ListView lvMsg;
+    String TAG="SMSRdInbox";
+
+    // Cursor Adapter
+    SimpleCursorAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_smsrd_inbox);
+
+        // Init GUI Widget
+        btnInbox = (Button) findViewById(R.id.btnInbox);
+        btnInbox.setOnClickListener(this);
+
+        btnSent = (Button)findViewById(R.id.btnSentBox);
+        btnSent.setOnClickListener(this);
+
+        btnDraft = (Button)findViewById(R.id.btnDraft);
+        btnDraft.setOnClickListener(this);
+
+        lvMsg = (ListView) findViewById(R.id.lvMsg);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == btnInbox) {
+
+            // Create Inbox box URI
+            Uri inboxURI = Uri.parse("content://sms/inbox");
+
+            // List required columns
+            String[] reqCols = new String[] { "_id", "address", "body" };
+
+            // Get Content Resolver object, which will deal with Content Provider
+            ContentResolver cr = getContentResolver();
+
+            // Fetch Inbox SMS Message from Built-in Content Provider
+            Cursor c = cr.query(inboxURI, reqCols, null, null, null);
+            c.moveToFirst();
+            Log.i(TAG, c.getString(c.getColumnIndex("address")));
+//            Log.i(TAG,"Test");
+            // Attached Cursor with adapter and display in listview
+            adapter = new SimpleCursorAdapter(this, R.layout.row, c,
+                    new String[] { "body", "address" }, new int[] {
+                    R.id.lblMsg, R.id.lblNumber });
+            lvMsg.setAdapter(adapter);
+
+        }
+
+        if(v==btnSent)
+        {
+
+            // Create Sent box URI
+            Uri sentURI = Uri.parse("content://sms/sent");
+
+            // List required columns
+            String[] reqCols = new String[] { "_id", "address", "body" };
+
+            // Get Content Resolver object, which will deal with Content Provider
+            ContentResolver cr = getContentResolver();
+
+            // Fetch Sent SMS Message from Built-in Content Provider
+            Cursor c = cr.query(sentURI, reqCols, null, null, null);
+            // Attached Cursor with adapter and display in listview
+            adapter = new SimpleCursorAdapter(this, R.layout.row, c,
+                    new String[] { "body", "address" }, new int[] {
+                    R.id.lblMsg, R.id.lblNumber });
+            lvMsg.setAdapter(adapter);
+
+        }
+
+        if(v==btnDraft)
+        {
+            // Create Draft box URI
+            Uri draftURI = Uri.parse("content://sms/draft");
+
+            // List required columns
+            String[] reqCols = new String[] { "_id", "address", "body" };
+
+            // Get Content Resolver object, which will deal with Content Provider
+            ContentResolver cr = getContentResolver();
+
+            // Fetch Sent SMS Message from Built-in Content Provider
+            Cursor c = cr.query(draftURI, reqCols, null, null, null);
+
+            // Attached Cursor with adapter and display in listview
+            adapter = new SimpleCursorAdapter(this, R.layout.row, c,
+                    new String[] { "body", "address" }, new int[] {
+                    R.id.lblMsg, R.id.lblNumber });
+            lvMsg.setAdapter(adapter);
+
+        }
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_smsrd_inbox, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
