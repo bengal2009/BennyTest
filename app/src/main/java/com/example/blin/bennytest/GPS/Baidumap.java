95b17885a3c351d6da4b082c34cd23f2e795455d
bengal2009 <bennylin@outlook.com>
2015-03-19 16:34:57 +0800

    office

--- app/src/main/java/com/example/blin/bennytest/GPS/Baidumap.java
+++ app/src/main/java/com/example/blin/bennytest/GPS/Baidumap.java
@@ -6,44 +6,54 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.VersionInfo;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
 import com.baidu.mapapi.map.MapStatusUpdate;
 import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
 import com.baidu.mapapi.map.MyLocationConfiguration;
 import com.baidu.mapapi.map.MyLocationConfiguration.LocationMode;
 import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.geocode.GeoCodeOption;
+import com.baidu.mapapi.search.geocode.GeoCodeResult;
+import com.baidu.mapapi.search.geocode.GeoCoder;
+import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
 import com.example.blin.bennytest.R;
 
-public class Baidumap extends Activity {
-
-
+public class Baidumap extends Activity implements
+        OnGetGeoCoderResultListener {
+    String TAG = "LocationDemo";
     // 定位相?
     LocationClient mLocClient;
     public MyLocationListenner myListener = new MyLocationListenner();
     private LocationMode mCurrentMode;
     BitmapDescriptor mCurrentMarker;
 
-    MapView mMapView;
-    BaiduMap mBaiduMap;
 
+    GeoCoder mSearch = null; // 搜索模?，也可去掉地?模??立使用
+    BaiduMap mBaiduMap = null;
+    MapView mMapView = null;
     // UI相?
     RadioGroup.OnCheckedChangeListener radioButtonListener;
     Button requestLocButton;
     boolean isFirstLoc = true;// 是否首次定位
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baidumap);
-
         requestLocButton = (Button) findViewById(R.id.button1);
         mCurrentMode = LocationMode.NORMAL;
         requestLocButton.setText("普通");
@@ -89,8 +99,8 @@ public class Baidumap extends Activity {
                 }
                 if (checkedId == R.id.customicon) {
                     // 修改?自定?marker
-                    mCurrentMarker = BitmapDescriptorFactory
-                            .fromResource(R.drawable.icon_geo);
+//					mCurrentMarker = BitmapDescriptorFactory
+//							.fromResource(R.drawable.icon_geo);
                     mBaiduMap
                             .setMyLocationConfigeration(new MyLocationConfiguration(
                                     mCurrentMode, true, mCurrentMarker));
@@ -101,6 +111,7 @@ public class Baidumap extends Activity {
 
         // 地?初始化
         mMapView = (MapView) findViewById(R.id.bmapView);
+
         mBaiduMap = mMapView.getMap();
         // ??定位??
         mBaiduMap.setMyLocationEnabled(true);
@@ -113,7 +124,18 @@ public class Baidumap extends Activity {
         option.setScanSpan(1000);
         mLocClient.setLocOption(option);
         mLocClient.start();
-
+        // 初始化搜索模?，注?事件?听
+        mSearch = GeoCoder.newInstance();
+        mSearch.setOnGetGeoCodeResultListener(this);
+    }
+    public void TEST(View V)
+    {
+        BDLocation location;
+        mSearch.geocode(new GeoCodeOption().city(
+                "上海").address(
+                "中山北路二段"));
+        Toast.makeText(getApplicationContext(), VersionInfo.getApiVersion(),
+                Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -131,17 +153,19 @@ public class Baidumap extends Activity {
                             // 此??置??者?取到的方向信息，???0-360
                     .direction(100).latitude(location.getLatitude())
                     .longitude(location.getLongitude()).build();
+            Log.i(TAG,"Latitude:"+Double.toString(location.getLatitude()));
+            Log.i(TAG,"Longitude:"+Double.toString(location.getLongitude()));
             mBaiduMap.setMyLocationData(locData);
             if (isFirstLoc) {
                 isFirstLoc = false;
                 LatLng ll = new LatLng(location.getLatitude(),
                         location.getLongitude());
+                Log.i(TAG,"Latitude:"+Double.toString(location.getLatitude()));
+                Log.i(TAG,"Longitude:"+Double.toString(location.getLongitude()));
+
+
                 MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
                 mBaiduMap.animateMapStatus(u);
-                Log.i("BAIDU", "Latitude:" + Double.toString(location.getLatitude()));
-                Log.i("BAIDU","Longtitude:"+Double.toString(location.getLongitude()));
-//                Toast.makeText(getApplicationContext(),location.getLongitude(),
-//                        Toast.LENGTH_SHORT).show();
             }
         }
 
@@ -171,5 +195,39 @@ public class Baidumap extends Activity {
         mMapView = null;
         super.onDestroy();
     }
+    @Override
+    public void onGetGeoCodeResult(GeoCodeResult result) {
+        if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+            Toast.makeText(this, "onGetGeoCodeResult抱歉", Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        mBaiduMap.clear();
+        mBaiduMap.addOverlay(new MarkerOptions().position(result.getLocation())
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.icon_marka)));
+        mBaiduMap.setMapStatus(MapStatusUpdateFactory.newLatLng(result
+                .getLocation()));
+        String strInfo = String.format("?度：%f ?度：%f",
+                result.getLocation().latitude, result.getLocation().longitude);
+        Toast.makeText(this, strInfo, Toast.LENGTH_LONG).show();
+    }
 
+    @Override
+    public void onGetReverseGeoCodeResult(ReverseGeoCodeResult result) {
+        if (result == null || result.error != SearchResult.ERRORNO.NO_ERROR) {
+            Toast.makeText(this, "Sorry! Not found!", Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        mBaiduMap.clear();
+        mBaiduMap.addOverlay(new MarkerOptions().position(result.getLocation())
+                .icon(BitmapDescriptorFactory
+                        .fromResource(R.drawable.icon_marka)));
+        mBaiduMap.setMapStatus(MapStatusUpdateFactory.newLatLng(result
+                .getLocation()));
+        Toast.makeText(this, result.getAddress(),
+                Toast.LENGTH_LONG).show();
+
+    }
 }
